994395e14bf629d9329345321fd801070f0a51a0
yifan-liao <lyyf927@qq.com>
2016-06-08 18:57:17 +0800

    Configure sass, autoprefixer, babel and their watch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,62 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
-        autoprefixer: {
-            dist: {
-                files: {
-                    'aqhi/static/css/home.css': 'aqhi/static/css/home.css'
-                }
-            }
-        },
-        watch: {
-            styles: {
-                files: ['aqhi/static/css/home.css'],
-                tasks: ['autoprefixer']
-            }
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          files: [{
+            expand: true,
+            cwd: 'styles',
+            src: ['aqhi/static/sass/*.sass'],
+            dest: 'aqhi/static/css',
+            ext: '.css'
+          }]
         }
-    });
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+      }
+    },
+    postcss: {
+      options: {
+        map: {
+          inline: false,
+          annotation: 'aqhi/static/css'
+        },
+        processors: [
+          require('autoprefixer')({
+            browsers: ['last 2 versions']
+          })
+        ]
+      },
+      dist: {
+        src: 'aqhi/static/css/*.css'
+      }
+    },
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['es2015']
+      },
+      dist: {
+        files: {
+          'aqhi/static/js/home-compiled.js': 'aqhi/static/js/home.js'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['aqhi/static/sass/*.sass'],
+        tasks: ['sass', 'postcss']
+      },
+      babel: {
+        files: 'aqhi/static/js/home.js',
+        tasks: 'babel'
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-babel');
+
+  grunt.registerTask('default', ['sass', 'postcss']);
 };
